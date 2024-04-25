1b1ba3a198676c51306b96d15dcae04ce7aa0a8e
Adrianna <adriannahatcher24@gmail.com>
2024-04-24 23:54:27 -0400

    Fill database and rename page

--- WebMvc/obj/Debug/net8.0/WebMvc.AssemblyInfo.cs
+++ WebMvc/obj/Debug/net8.0/WebMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec937ccdfa641cfdfe7e1c542cf22a273791fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
